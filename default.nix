a119a07983cf24db0bf9715c5625ebb90d915f16
Ethel Morgan <eth@ethulhu.co.uk>
2020-06-20 00:12:11 +0100

    add Nix build

---
+++ default.nix
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2020 Ethel Morgan
+#
+# SPDX-License-Identifier: MIT
+
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+buildGoModule rec {
+  name = "catbus-actuator-wakeonlan-${version}";
+  version = "latest";
+  goPackagePath = "go.eth.moe/catbus-actuator-wakeonlan";
+
+  modSha256 = "0nj0ny9692bqcw04fh74g8hqgfh3qc095fsq0y9cy677kp7l2q94";
+
+  src = ./.;
+
+  meta = {
+    homepage = "https://ethulhu.co.uk/catbus";
+    licence = stdenv.lib.licenses.mit;
+  };
+}
